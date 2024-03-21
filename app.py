0591b6ed90b57e3330267e692f2eef1acaa8d4fc
Shatha <155658996+ShathAI1@users.noreply.github.com>
2024-03-21 11:06:06 +0000

    recieve message and save to db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import load_projects_from_db
+from flask import Flask, render_template, request
+from database import load_projects_from_db, load_project_from_db, add_message_to_db
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,20 @@ def hello_world():
   return render_template('home.html', myskills=projects, 
                          myname = 'Shatha')
 
+@app.route("/project/<id>")
+def show_project(id):
+  project = load_project_from_db(id)
+  return render_template('projectdetails.html', project=project)
 
+@app.route("/contactme")
+def contactpage():
+  return render_template('contactme.html')
+
+@app.route("/message",methods = ['post'])
+def messageme():
+  data = request.form
+  add_message_to_db(data)
+  return render_template('message_submitted.html',message=data)
+    
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=True)  # run flask server
